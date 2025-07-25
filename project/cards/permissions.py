041f781fc46b2d987da9465c95d824326da1b3fd
abdelmohsen22 <abdelmohsen.thabet@yahoo.com>
2025-07-25 09:42:58 +0300

    f

--- project/cards/permissions.py
+++ project/cards/permissions.py
@@ -8,4 +8,4 @@ class IsAdminOrCreateOnly(permissions.BasePermission):
     def has_permission(self, request ,view):
         if request.method in ['GET', 'POST']:
             return request.user and request.user.is_authenticated
-        return request.user and request.user.is_staff
+        return request.user and request.user.is_staff 
